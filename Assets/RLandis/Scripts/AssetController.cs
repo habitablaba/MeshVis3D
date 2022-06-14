f0dfcf12b889473154e1fb90e31bf0ee21816541
habitablaba <ryanoflandis@gmail.com>
2022-06-13 19:23:43 -0500

    Project creation and Initial Visualizer functionality
    * Load all assets from Resources subfolder
    * Allow selection of Prefab, Mesh, Material, and Texture assets
    * Transform tools: translate, rotate, scale
    * Rudimentary UI

---
+++ Assets/RLandis/Scripts/AssetController.cs
@@ -0,0 +1,117 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class AssetController : MonoBehaviour
+{
+    [SerializeField] private UIDocument _uiDocument;
+    [SerializeField] private ModelViewer _modelViewerPrefab;
+
+    private ModelViewer _modelViewerInstance;
+
+    private List<Object> _assets;
+    private List<Mesh> _models;
+    private List<Texture> _textures;
+    private List<Material> _materials;
+    private List<GameObject> _prefabs;
+
+    private VisualElement _rootVisualElement;
+    private DropdownField _prefabDropdown;
+    private DropdownField _modelDropdown;
+    private DropdownField _materialDropdown;
+    private DropdownField _textureDropdown;
+
+    private void Awake()
+    {
+        ReloadAssets();
+    }
+
+    private void OnEnable()
+    {
+        SetupUI();
+    }
+
+    private void OnDisable()
+    {
+        _prefabDropdown.UnregisterValueChangedCallback(OnPrefabSelectionChanged);
+        _modelDropdown.UnregisterValueChangedCallback(OnModelSelectionChanged);
+        _materialDropdown.UnregisterValueChangedCallback(OnMaterialSelectionChanged);
+        _textureDropdown.UnregisterValueChangedCallback(OnTextureSelectionChanged);
+    }
+
+    private void Start()
+    {
+        _modelViewerInstance = Instantiate(_modelViewerPrefab);
+    }
+
+    /// <summary>
+    /// All the resources are loaded, then all the prefabs are pulled from that.
+    /// Viewing a model starts with selecting a prefab
+    /// </summary>
+    private void ReloadAssets()
+    {
+        _assets = Resources.LoadAll("MeshVis").ToList();
+        _prefabs = _assets.Where(o => o is GameObject).Select(o => o as GameObject).ToList();
+        _models = new List<Mesh>();
+        _materials = new List<Material>();
+        _textures = new List<Texture>();
+    }
+
+    private void SetupUI()
+    {
+        _rootVisualElement = _uiDocument.rootVisualElement;
+
+        _prefabDropdown = _rootVisualElement.Q<DropdownField>("prefab-dropdown");
+        _modelDropdown = _rootVisualElement.Q<DropdownField>("model-dropdown");
+        _materialDropdown = _rootVisualElement.Q<DropdownField>("material-dropdown");
+        _textureDropdown = _rootVisualElement.Q<DropdownField>("texture-dropdown");
+
+        _prefabDropdown.Q<Label>().text = "Prefab";
+        _modelDropdown.Q<Label>().text = "Model";
+        _materialDropdown.Q<Label>().text = "Material";
+        _textureDropdown.Q<Label>().text = "Texture";
+
+        _prefabDropdown.RegisterValueChangedCallback(OnPrefabSelectionChanged);
+        _modelDropdown.RegisterValueChangedCallback(OnModelSelectionChanged);
+        _materialDropdown.RegisterValueChangedCallback(OnMaterialSelectionChanged);
+        _textureDropdown.RegisterValueChangedCallback(OnTextureSelectionChanged);
+
+        _materials = _assets.Where(o => o is Material).Select(o => o as Material).ToList();
+        _textures = _assets.Where(o => o is Texture).Select(o => o as Texture).ToList();
+
+        _prefabDropdown.choices = _prefabs.Select(o => o.name).ToList();
+        _materialDropdown.choices = _materials.Select(o => o.name).ToList();
+        _textureDropdown.choices = _textures.Select(o => o.name).ToList();
+
+    }
+
+    private void OnPrefabSelectionChanged(ChangeEvent<string> changeEvent)
+    {
+        var selection = _prefabs.FirstOrDefault(p => p.name == changeEvent.newValue);
+        _modelViewerInstance.Show(selection);
+
+        _models = selection.GetComponentsInChildren<MeshFilter>().Select(mf => mf.sharedMesh).ToList();
+        _modelDropdown.choices = _models.Select(o => o.name).ToList();
+
+        _modelDropdown.index = -1;
+        _materialDropdown.index = -1;
+        _textureDropdown.index = -1;
+    }
+
+    private void OnModelSelectionChanged(ChangeEvent<string> changeEvent)
+    {
+        _materialDropdown.index = -1;
+        _textureDropdown.index = -1;
+    }
+
+    private void OnMaterialSelectionChanged(ChangeEvent<string> changeEvent)
+    {
+        _modelViewerInstance.SetMaterial(_materials.FirstOrDefault(m => m.name == changeEvent.newValue));
+    }
+
+    private void OnTextureSelectionChanged(ChangeEvent<string> changeEvent)
+    {
+        _modelViewerInstance.SetTexture(_textures.FirstOrDefault(t => t.name == changeEvent.newValue));
+    }
+}
---
+++ Assets/RLandis/Scripts/ModelViewer.cs
@@ -0,0 +1,159 @@
+using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+/// <summary>
+/// Displaying models is done by creating a new game object
+/// to act as an anchor for the model and any transform operations the user
+/// may perform.
+/// </summary>
+public class ModelViewer : MonoBehaviour
+{
+    public enum Mode
+    {
+        None = -1,
+        Transform,
+        Rotate,
+        Scale
+    }
+
+    private readonly Type[] _meshTypes = new Type[]
+    {
+        typeof(MeshFilter),
+        typeof(MeshRenderer),
+    };
+
+    private readonly Type[] _containerTypes = new Type[]
+    {
+        typeof(BoxCollider),
+    };
+
+    [SerializeField] private Material _defaultMaterial;
+    [SerializeField] private UIDocument _uiDocument;
+
+    private GameObject _modelContainer;
+    private GameObject _modelObject;
+    private VisualElement _rootVisualElement;
+    private RadioButtonGroup _modeSelectRadio;
+    private Mode _selectedMode = Mode.Transform;
+    private Camera _mainCamera;
+    private float _cameraToObjectDistance;
+    private Button _resetButton;
+    private Quaternion _initialRotation;
+
+
+    public void Show(GameObject showThis)
+    {
+        NewModelContainer();
+        _modelObject = Instantiate(showThis);
+        _modelObject.transform.SetParent(_modelContainer.transform, false);
+    }
+
+    public void SetMaterial(Material material)
+    {
+        if (material == null) return;
+    }
+
+    public void SetTexture(Texture texture)
+    {
+        if (texture == null) return;
+    }
+
+    private void Awake()
+    {
+        // later, this may be load last model, etc.
+        NewModelContainer();
+        _mainCamera = Camera.main;
+
+        _cameraToObjectDistance = _mainCamera.WorldToScreenPoint(_modelContainer.transform.position).z;
+    }
+
+    private void OnEnable()
+    {
+        SetupUI();
+    }
+
+    private void OnDisable()
+    {
+        _modeSelectRadio.UnregisterValueChangedCallback(OnModeSelectValueChanged);
+    }
+
+    private void OnMouseDown()
+    {
+        _initialRotation = _modelContainer.transform.rotation;
+    }
+
+    private void OnMouseDrag()
+    {
+        var mouseWorldPosition = _mainCamera.ScreenToWorldPoint(new Vector3(
+            Input.mousePosition.x,
+            Input.mousePosition.y,
+            _cameraToObjectDistance));
+
+        var objectToMouse = mouseWorldPosition - _modelContainer.transform.position;
+
+        switch (_selectedMode)
+        {
+            case Mode.Transform:
+                _modelContainer.transform.position += objectToMouse;
+                break;
+            case Mode.Rotate:
+                // multiplying here to make rotation feel more snappy.
+                var rotationVector = 10 * objectToMouse;
+                // todo; there has to be a better way.
+                var newRotation = Quaternion.Euler(
+                    rotationVector.y,
+                    -rotationVector.x,
+                    rotationVector.z);
+                _modelContainer.transform.rotation = _initialRotation * newRotation;
+                break;
+            case Mode.Scale:
+                _modelContainer.transform.localScale = Vector3.one * objectToMouse.magnitude;
+                break;
+            default:
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Nuke whatever we have and make a new one.
+    /// todo; later, there could be options for loading from a template
+    ///         container instead of just starting from scorched earth.
+    /// todo; later still, there could be a container factory
+    ///         that takes care of all of the lifecycle management.
+    /// </summary>
+    private void NewModelContainer()
+    {
+        Destroy(_modelContainer);
+        _modelContainer = new GameObject("ModelContainer", _containerTypes);
+        _modelContainer.transform.SetParent(transform, false);
+        var collider = _modelContainer.GetComponent<BoxCollider>();
+        collider.size = Vector3.one * 4;
+        collider.center = Vector3.up;
+    }
+
+    private void SetupUI()
+    {
+        _rootVisualElement = _uiDocument.rootVisualElement;
+        _modeSelectRadio = _rootVisualElement.Q<RadioButtonGroup>("mode-select-radio");
+        _modeSelectRadio.Q<Label>().text = "Mode";
+        _modeSelectRadio.RegisterValueChangedCallback(OnModeSelectValueChanged);
+        _modeSelectRadio.SetValueWithoutNotify((int)_selectedMode);
+
+        _resetButton = _rootVisualElement.Q<Button>("reset-view-button");
+
+        _resetButton.clicked += OnResetButtonClicked;
+    }
+
+    private void OnResetButtonClicked()
+    {
+        _modelContainer.transform.position = Vector3.zero;
+        _modelContainer.transform.rotation = Quaternion.identity;
+        _modelContainer.transform.localScale = Vector3.one;
+    }
+
+    private void OnModeSelectValueChanged(ChangeEvent<int> changeEvent)
+    {
+        _selectedMode = (Mode)changeEvent.newValue;
+    }
+}
